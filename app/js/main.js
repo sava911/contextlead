04e77e0635c2f733764f2d45c82d34b151fcee57
sava911 <pesss929@gmail.com>
2024-07-09 02:32:48 +0300

    end main, start modal up dawn

--- app/js/main.js
+++ app/js/main.js
@@ -75,6 +75,12 @@ $(function () {
   }
 
 
+  document.querySelectorAll(".accordion-item").forEach((item) => {
+    item.querySelector(".accordion-item-header").addEventListener("click", () => {
+      item.classList.toggle("open");
+    });
+  });
+
 
 
 
